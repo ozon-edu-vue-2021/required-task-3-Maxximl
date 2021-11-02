ece05352ab724263b1794687819ca894b810ca34
Maxim Lavrinenko <max1mlavrinenko@yandex.ru>
2021-11-02 11:22:10 +0300

    Add required functional

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map @selectTable="handleTableSelect" />
+      <SideMenu v-bind.sync="sideMenuData" />
     </div>
+  </div>
 </template>
 
 <script>
 import Map from "./components/Map.vue";
 import SideMenu from "./components/SideMenu.vue";
+import people from "./assets/data/people.json";
+import { find } from "lodash";
 
 export default {
   name: "App",
@@ -17,40 +19,70 @@ export default {
     Map,
     SideMenu,
   },
+  data: function () {
+    return {
+      sideMenuData: {
+        isUserOpenned: false,
+        person: null,
+      },
+      people: null,
+    };
+  },
+  created() {
+    this.people = people;
+  },
+  mounted() {
+    document.addEventListener("click", this.handleOutsideClick);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleOutsideClick);
+  },
+  methods: {
+    handleTableSelect(table) {
+      const personData = find(this.people, (man) => man.tableId === table._id);
+      this.sideMenuData.person = personData;
+      this.sideMenuData.isUserOpenned = true;
+    },
+    handleOutsideClick(event) {
+      if (!event.target.classList.contains("wrapper-table")) {
+        this.sideMenuData.isUserOpenned = false;
+      }
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
